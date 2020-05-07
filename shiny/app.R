28c4ed9aec29894dcfcd677a98c103e4065a80e7
Sol Represa <solrepresa@users.noreply.github.com>
2020-05-07 17:27:36 -0300

    ADD: Shiny app for COVID model

---
+++ shiny/app.R
@@ -0,0 +1,247 @@
+# App COVID SEIcIscR
+
+library(shiny)
+
+
+### CARGAMOS DATA DE COVID  ###
+
+source('codes/function_modelSEIcIscR.r')
+source('codes/function_postprocessing.r')
+# source('codes/function_plots_simOutbreak.r')
+source('codes/ARG_Data.r')
+source('codes/1_loadData_Arg.r')
+
+
+
+ui <- fluidPage(
+
+  titlePanel("COVID models"),
+  p('Modelo SEIcIscR'),
+  hr(),
+  
+  wellPanel(
+    fluidRow(
+      column(6,
+             h3("General parameters"),
+             
+             ##     NuMBER SIMULATION     ##
+             #sliderInput(inputId = "nsim",
+             #            label = "Choose a number of simulations",
+             #            value = 2, min = 1, max = 200, step = 10),
+             
+             ##     PARAMETERS     ##
+             numericInput("durInfSim", label = "Duration of infection (days)", value = 7),
+             
+             numericInput("initialI", label = "Number of initial infected", value = 35),
+             
+             hr(),
+             
+             h3("Without intervention"),
+             
+             dateInput("dateSII", label = "Date Start Intense Intervention", value = "2020-03-19"),
+             
+             dateInput("dateSSC", label = "Date Start School Closure", value = "2020-03-19")
+             ),
+      
+      column(6,
+             ## CASO 1
+             h3("Case 1"),
+             dateInput("dateEII1", label = "Date End Intense Intervention", value = "2020-04-10"),
+             
+             h4("Number of stagger weeks"),
+             sliderInput('numWeekStagger_11', 'First change', 2, min = 0, max = 10, step = 1),
+             sliderInput('numWeekStagger_12', 'Second change', 4, min = 0, max = 10, step = 1),
+             sliderInput('numWeekStagger_13', 'Third change', 6, min = 0, max = 10, step = 1),
+             
+             h4("Proportion of the work force that is working"),
+             sliderInput('WorkOpen_11', 'Initial Period', 1, min = 0, max = 1, step = 0.25),
+             sliderInput('WorkOpen_12', 'Second Period', 1, min = 0, max = 1, step = 0.25),
+             sliderInput('WorkOpen_13', 'Third Period', 1, min = 0, max = 1, step = 0.25),
+             sliderInput('WorkOpen_14', 'Fourth period', 1, min = 0, max = 1, step = 0.25)
+      ),
+      
+      column(6,
+             ## CASO 2
+             hr(),
+             h3("Case 2"),
+             dateInput("dateEII2", label = "Date End Intense Intervention", value = "2020-04-24"),
+
+             h4("Number of stagger weeks"),
+             sliderInput('numWeekStagger_21', 'First change', 2, min = 0, max = 10, step = 1),
+             sliderInput('numWeekStagger_22', 'Second change', 4, min = 0, max = 10, step = 1),
+             sliderInput('numWeekStagger_23', 'Third change', 6, min = 0, max = 10, step = 1),
+             
+             h4("Proportion of the work force that is working"),
+             sliderInput('WorkOpen_21', 'Initial period', 1, min = 0, max = 1, step = 0.25),
+             sliderInput('WorkOpen_22', 'Second period', 1, min = 0, max = 1, step = 0.25),
+             sliderInput('WorkOpen_23', 'Third period', 1, min = 0, max = 1, step = 0.25),
+             sliderInput('WorkOpen_24', 'Fourth period', 1, min = 0, max = 1, step = 0.25)
+             
+      ),
+      column(6,
+             ## CASO 3
+             hr(),
+             h3("Case 3"),
+             dateInput("dateEII3", label = "Date End Intense Intervention", value = "2020-05-10"),
+
+             h4("Number of stagger weeks"),
+             sliderInput('numWeekStagger_31', 'First change', 2, min = 0, max = 10, step = 1),
+             sliderInput('numWeekStagger_32', 'Second change', 4, min = 0, max = 10, step = 1),
+             sliderInput('numWeekStagger_33', 'Third change', 6, min = 0, max = 10, step = 1),
+             
+             h4("Proportion of the work force that is working"),
+             sliderInput('WorkOpen_31', 'Proportion of the work', 1, min = 0, max = 1, step = 0.25),
+             sliderInput('WorkOpen_32', 'Proportion of the work', 1, min = 0, max = 1, step = 0.25),
+             sliderInput('WorkOpen_33', 'Proportion of the work', 1, min = 0, max = 1, step = 0.25),
+             sliderInput('WorkOpen_34', 'Proportion of the work', 1, min = 0, max = 1, step = 0.25)
+             )
+    )
+  ),
+  
+  mainPanel(
+    h3("New daily infected"),
+    plotOutput("modelplot")
+  )
+
+)
+
+
+
+
+
+server <- function(input, output){
+  output$modelplot <- renderPlot({
+    
+    nsim = 10
+  
+    R0est <- sample(x = r0posterior, size = nsim)
+    
+    ### COMIENZA 'codes/2_simOutbreak_ncov_SEIcIscR_ARG.r' ###
+    
+    set.seed(123)
+  
+    
+    epi_doNothing_l = vector('list', nsim)
+    epi_base_l = vector('list', nsim)
+    epi_march_l = vector('list', nsim)
+    epi_april_l = vector('list', nsim)
+    
+    
+    for(sim in 1:nsim)
+    {
+      epi_doNothing <- function(){simulateOutbreakSEIcIscR(R0t = R0est[sim],
+                                                        R0tpostoutbreak = 1.17,
+                                                        rho = c(rep(0.4,4),rep(0.8,12)),
+                                                        dateStart = as.Date('2020-03-05'),
+                                                        dateStartSchoolClosure = as.Date(input$dateSSC),
+                                                        dateStartIntenseIntervention = as.Date(input$dateSII), 
+                                                        dateEndIntenseIntervention = as.Date(input$dateSII),  #date we begin relaxing intense intervention
+                                                        pWorkOpen = c(1, 1, 1, 1),   # pWorkOpen: proportion of the work force that is working (will be time-varying)
+                                                        numWeekStagger = c(0,0,0),
+                                                        pInfected = input$initialI / 1000000,
+                                                        durInf = input$durInfSim)}
+      
+      epi_base <- function(){simulateOutbreakSEIcIscR(R0t = R0est[sim],
+                                             R0tpostoutbreak = 1.17,
+                                             rho = c(rep(0.4,4),rep(0.8,12)),
+                                             dateStart = as.Date('2020-03-05'),
+                                             dateStartSchoolClosure = as.Date(input$dateSSC),
+                                             dateStartIntenseIntervention = as.Date(input$dateSII), 
+                                             dateEndIntenseIntervention = as.Date(input$dateEII1),
+                                             pWorkOpen = c(input$WorkOpen_11, input$WorkOpen_12, input$WorkOpen_13, input$WorkOpen_14),
+                                             numWeekStagger = c(input$numWeekStagger_11,input$numWeekStagger_12, input$numWeekStagger_13),
+                                             pInfected = input$initialI / 1000000,
+                                             durInf = input$durInfSim)}
+      
+      epi_march <- function(){simulateOutbreakSEIcIscR(R0t = R0est[sim],
+                                              R0tpostoutbreak = 1.17,
+                                              rho = c(rep(0.4,4),rep(0.8,12)),
+                                              dateStart = as.Date('2020-03-05'),
+                                              dateStartSchoolClosure = as.Date(input$dateSSC),
+                                              dateStartIntenseIntervention = as.Date(input$dateSII), 
+                                              dateEndIntenseIntervention = as.Date(input$dateEII2),
+                                              pWorkOpen = c(input$WorkOpen_21, input$WorkOpen_22, input$WorkOpen_23, input$WorkOpen_24),
+                                              numWeekStagger = c(input$numWeekStagger_21,input$numWeekStagger_22, input$numWeekStagger_23),
+                                              pInfected = input$initialI / 1000000,
+                                              durInf = input$durInfSim)}
+      
+      epi_april <- function(){simulateOutbreakSEIcIscR(R0t = R0est[sim],
+                                              R0tpostoutbreak = 1.17,
+                                              rho = c(rep(0.4,4),rep(0.8,12)),
+                                              dateStart = as.Date('2020-03-05'),
+                                              dateStartSchoolClosure = as.Date(input$dateSSC),
+                                              dateStartIntenseIntervention = as.Date(input$dateSII), 
+                                              dateEndIntenseIntervention = as.Date(input$dateEII3),
+                                              pWorkOpen = c(input$WorkOpen_31, input$WorkOpen_32, input$WorkOpen_33, input$WorkOpen_34),
+                                              numWeekStagger = c(input$numWeekStagger_31,input$numWeekStagger_32, input$numWeekStagger_33),
+                                              pInfected = input$initialI / 1000000,
+                                              durInf = input$durInfSim)}
+      
+      epi_doNothing_l[[sim]] <- epi_doNothing()
+      epi_base_l[[sim]] <- epi_base()
+      epi_march_l[[sim]] <- epi_march()
+      epi_april_l[[sim]] <- epi_april()
+      
+      
+      #if(sim%%10==0) print(paste0('Done with simulation ',sim))
+  
+    }
+    
+    covid_I = list() 
+    covid_I[[1]] <- function(){ summariseSimulations(VAR = 'incidence',CI = 50,SIMS = epi_doNothing_l)}
+    covid_I[[2]] <- function(){ summariseSimulations(VAR = 'incidence',CI = 50,SIMS = epi_base_l)}
+    covid_I[[3]] <- function(){ summariseSimulations(VAR = 'incidence',CI = 50,SIMS = epi_march_l)}
+    covid_I[[4]] <- function(){ summariseSimulations(VAR = 'incidence',CI = 50,SIMS = epi_april_l)}
+    
+    covid_DurInf = list() 
+    covid_DurInf[[1]] <- function(){ summariseSimulations_mid(CI = 50,SIMS = epi_doNothing_l)}
+    covid_DurInf[[2]] <- function(){ summariseSimulations_mid(CI = 50,SIMS = epi_base_l)}
+    covid_DurInf[[3]] <- function(){ summariseSimulations_mid(CI = 50,SIMS = epi_march_l)}
+    covid_DurInf[[4]] <- function(){ summariseSimulations_mid(CI = 50,SIMS = epi_april_l)}
+    
+    
+    ## EL PLOT  ##
+    
+    legends = c("Sin intervencion", "Caso 1", "Caso 2", "Caso 3")
+    l = c("Mediana", "Quartil Inferior",  "Quartil Superior")
+    
+
+    par(par(no.readonly=TRUE))
+    par(mfrow=c(2,2), oma=c(4,2,2,4), mar=c(3,3,2,0), pch=16)
+    
+    for( i in 1:4){
+      plot(covid_DurInf[[4]]()[["S"]][["time"]]/7, 
+           covid_I[[i]]()[["summary"]][["uci"]],
+           xlim=c(0, 61),    # ~428 days
+           #  ylim=c(0,70000),
+           type="l", 
+           col="red",
+           main= legends[i],
+           xlab="",
+           ylab="")
+      lines(covid_DurInf[[4]]()[["S"]][["time"]]/7, 
+            covid_I[[i]]()[["summary"]][["lci"]], 
+            col="green")
+      lines(covid_DurInf[[4]]()[["S"]][["time"]]/7, 
+            covid_I[[i]]()[["summary"]][["median"]], 
+            col="black")
+      legend("topleft" , legend= l,
+             col=c("black", "green", "red"), 
+             lty=2:1,
+             bty='n',
+             cex = 1)
+      mtext(text = "Infectados c/24hs", side=2,line=0,outer=TRUE)
+      mtext("Día final de las intervenciones", side=1,line=2, outer=TRUE, adj = 1)
+      mtext(text = paste("Caso 1:", covid_I[[1]]()[["Sim1"]][["dateEndIntenseIntervention"]],
+                         "- Caso 2:", covid_I[[2]]()[["Sim1"]][["dateEndIntenseIntervention"]],
+                         "- Caso 3:", covid_I[[3]]()[["Sim1"]][["dateEndIntenseIntervention"]],
+                         "- Caso 4:", covid_I[[4]]()[["Sim1"]][["dateEndIntenseIntervention"]]), 
+            side=1,line=3, outer=TRUE, adj = 1)
+      
+      }
+    
+  })
+}
+
+
+shinyApp( ui = ui, server = server)
